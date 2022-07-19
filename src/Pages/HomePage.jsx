69a706a199b32f664515d18d7e72cc66c0e27a1d
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-07-19 21:46:10 +0530

    Navbar half done

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Components/HomePage/Navbar/Navbar'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default HomePage

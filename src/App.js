515a7941b167add4d9d8f80ad5c74e57cac6e840
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-07-21 21:04:51 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Pricing from './components/Pricing/Pricing';
-import Why_harvest from './components/why harvest/Why_harvest';
-
+import Why_harvest from './Components/whyharvest/Why_harvest';
+import Pricing from "./Components/Pricing/Pricing";
 function App() {
   return (
     <div className="App">
-     <Why_harvest/>
+   
     </div>
   );
 }

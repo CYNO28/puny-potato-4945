4b4edc0e12a20890189c1b9e5b4f0028fbf96ff6
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2022-07-24 10:14:05 +0530

    1

--- Frontend/src/Pages/User/User.jsx
+++ Frontend/src/Pages/User/User.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar2 from '../../Components/navbar2/Navbar2'
 import {Routes,Route} from 'react-router-dom'
 import Expenses from '../../Components/Expenses/Expenses'
+import axios from 'axios'
 
-const User = ({user}) => {
-  console.log(user)
+const User = () => {
+  
+  const [user, setUser] = useState(null);
+
+
+
+	const getUser = async () => {
+		try {
+			const url = `https://punypotato4945.herokuapp.com/auth/login/success`;
+			const { data } = await axios.get(url, { withCredentials: true });
+			setUser(data.user._json);
+      console.log(data.user._json)
+      fetch(url)
+      .then(res => res.json())
+      .then(data => console.log(data)).catch(err => console.log(err))
+	
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	useEffect(() => {
+		getUser();
+	}, []);
   return (
 <>
 <Navbar2 user={user}></Navbar2>
